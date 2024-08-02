5ee531232fc7e85c43c461016eb91e385c6017c8
jeongyun-kim <kkanyo04@naver.com>
2024-08-02 20:15:23 +0900

    NicknameVC 구현
    닉네임이 2글자 이상 8글자 이하일 때 아닐 때, 버튼 색상 및 화면전환 가능 여부 변경

--- PracticeRxSwift/0801/SignUpOptional/NicknameViewController.swift
+++ PracticeRxSwift/0801/SignUpOptional/NicknameViewController.swift
@@ -7,32 +7,22 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
-class NicknameViewController: UIViewController {
-   
-    let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
-    let nextButton = PointButton(title: "다음")
+final class NicknameViewController: BaseViewController {
+    private let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
+    private let nextButton = PointButton(title: "다음")
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let disposeBag = DisposeBag()
+    private let bgColor = BehaviorRelay(value: UIColor.lightGray)
 
-        view.backgroundColor = Color.white
-        
-        configureLayout()
-       
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
-
-    }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(BirthdayViewController(), animated: true)
-    }
-
-    
-    func configureLayout() {
+    override func setupHierarchy() {
         view.addSubview(nicknameTextField)
         view.addSubview(nextButton)
-         
+    }
+    
+    override func setupConstraints() {
         nicknameTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
@@ -45,5 +35,30 @@ class NicknameViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
-
+    
+    override func setupUI() {
+        super.setupUI()
+    }
+    
+    override func bind() {
+        // 다음 버튼 누르면 생일뷰로
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(BirthdayViewController(), animated: true)
+            }.disposed(by: disposeBag)
+        
+        // 버튼 배경색 변경 
+        bgColor
+            .bind(to: nextButton.rx.backgroundColor)
+            .disposed(by: disposeBag)
+         
+        // 닉네임이 2글자 이상 8글자 이하라면
+        nicknameTextField.rx.text.orEmpty
+            .map { 2 <= $0.count && $0.count <= 8}
+            .bind(with: self) { owner, isValid in
+                let color: UIColor = isValid ? .systemGreen : .lightGray
+                owner.bgColor.accept(color)
+                owner.nextButton.isEnabled = isValid
+            }.disposed(by: disposeBag)
+    }
 }
