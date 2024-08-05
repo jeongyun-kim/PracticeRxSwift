b009212e54809d2151e1adae36245415a6a99f5d
jeongyun-kim <kkanyo04@naver.com>
2024-08-05 23:52:56 +0900

    Refactor: ShoppingVC Input/Output Pattern MVVM 적용

--- PracticeRxSwift/0801/Shopping/ShoppingViewController.swift
+++ PracticeRxSwift/0801/Shopping/ShoppingViewController.swift
@@ -17,8 +17,7 @@ final class ShoppingViewController: BaseViewController {
     private let tableView = UITableView()
     
     private let disposeBag = DisposeBag()
-    private var list = ItemList().list
-    private var items = BehaviorRelay(value: ItemList().list)
+    private let vm = ShoppingViewModel()
     
     override func setupHierarchy() {
         view.addSubview(contentView)
@@ -72,28 +71,42 @@ final class ShoppingViewController: BaseViewController {
     }
     
     override func bind() {
+        let addItem = addButton.rx.tap.withLatestFrom(itemTextField.rx.text.orEmpty)
+        let removeItem = tableView.rx.itemDeleted
+        let renameItem = PublishRelay<(Int, String)>()
+        let likeItem = PublishRelay<Int>()
+        let bookmarkItem = PublishRelay<Int>()
+        let selectedItem = Observable
+            .zip(tableView.rx.itemSelected, tableView.rx.modelSelected(Item.self))
+        let searchBtnTapped = navigationItem.rightBarButtonItem?.rx.tap
+        let getNewItemList = PublishRelay<[Item]>()
+        
+        let input = ShoppingViewModel.Input(addItem: addItem, removeItem: removeItem, 
+                                            renameItem: renameItem, likeItem: likeItem,
+                                            bookmarkItem: bookmarkItem, selectedItem: selectedItem,
+                                            searchBtnTapped: searchBtnTapped, getNewItemList: getNewItemList)
+        guard let output = vm.transform(input) else { return }
+        
         // 테이블뷰 셀 그리기
-        items.bind(to: tableView.rx.items(cellIdentifier: ShoppingTableViewCell.identifier, cellType: ShoppingTableViewCell.self)) { (row, element, cell) in
+        output.items
+            .bind(to: tableView.rx.items(cellIdentifier: ShoppingTableViewCell.identifier, cellType: ShoppingTableViewCell.self)) { (row, element, cell) in
             cell.configureCell(element)
-            // 완료 버튼 눌렀을 때
-            cell.completeButton.rx.tap
-                .bind(with: self) { owner, _ in
-                    owner.list[row].isComplete.toggle()
-                    owner.items.accept(owner.list)
-                }.disposed(by: cell.disposeBag)
+            // 완료 버튼 눌렀을 때 ->
+                cell.completeButton.rx.tap
+                    .bind { _ in
+                        likeItem.accept(row)
+                    }.disposed(by: cell.disposeBag)
             // 즐겨찾기 버튼 눌렀을 때
-            cell.bookmarkButton.rx.tap
-                .bind(with: self) { owner, _ in
-                    owner.list[row].bookmark.toggle()
-                    owner.items.accept(owner.list)
-                }.disposed(by: cell.disposeBag)
+                cell.bookmarkButton.rx.tap
+                    .bind { _ in
+                        bookmarkItem.accept(row)
+                    }.disposed(by: cell.disposeBag)
         }.disposed(by: disposeBag)
         
         // 테이블뷰 셀 선택 시 상세정보뷰 불러오기
         // - 상세정보로 현재 상품명 보내주기 
         // - 상세정보에서 상품명 수정하고 돌아오면 리스트 업데이트
-        Observable
-            .zip(tableView.rx.itemSelected, tableView.rx.modelSelected(Item.self))
+        output.selectedItem
             .subscribe(with: self) { owner, value in
                 // 상세정보뷰로 현재 선택한 상품명 보내기
                 let itemName = value.1.name
@@ -101,45 +114,35 @@ final class ShoppingViewController: BaseViewController {
                 vc.itemName.accept(itemName)
                 // 상세정보에서 상품명을 수정했다면 수정한 이름으로 데이터 재구성
                 vc.sendItem = { newItemName in
-                    owner.list[value.0.row].name = newItemName
-                    owner.items.accept(owner.list)
+                    renameItem.accept((value.0.row, newItemName))
                 }
                 owner.navigationController?.pushViewController(vc, animated: true)
             }.disposed(by: disposeBag)
+
+        // 아이템 추가했을 때 + 기본 상태일 때 텍스트 필드 텍스트 비워두기 
+        output.addedItemTrigger
+            .map { "" }
+            .bind(to: itemTextField.rx.text)
+            .disposed(by: disposeBag)
         
-        // 우측에서 좌측으로 스와이프 -> 아이템 삭제 
-        tableView.rx.itemDeleted
-            .bind(with: self) { owner, indexPath in
-                owner.list.remove(at: indexPath.row)
-                owner.items.accept(owner.list)
+        output.searchBtnTapped
+            .bind(with: self) { owner, _ in
+                let vc = ShoppingSearchViewController()
+                // ⚠️ list를 막 변경해줘도 여기서는 원래 상태의 list가 날아감
+                // => 좀 더 굴려볼 것
+                vc.list = output.items.value
+                vc.sendList = { [weak self] list in
+                    guard let self else { return }
+                    getNewItemList.accept(list)
+                }
+                owner.navigationController?.pushViewController(vc, animated: true)
             }.disposed(by: disposeBag)
-        
-        // 추가버튼 눌렀을 때 아이템 추가
-        addButton.rx.tap
-            .withLatestFrom(itemTextField.rx.text.orEmpty)
-            .bind(with: self) { owner, itemName in
-                guard !itemName.trimmingCharacters(in: .whitespaces).isEmpty else { return }
-                let item = Item(name: itemName)
-                owner.list.insert(item, at: 0)
-                owner.items.accept(owner.list)
-                owner.itemTextField.text = ""
-            }.disposed(by: disposeBag)
+            
     }
     
     private func configureNavigation() {
         navigationItem.title = "쇼핑"
-        let search = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(searchBtnTapped))
+        let search = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: nil)
         navigationItem.rightBarButtonItem = search
-    }
-    
-    @objc private func searchBtnTapped(_ sender: UIButton) {
-        let vc = ShoppingSearchViewController()
-        vc.list = list
-        vc.sendList = { [weak self] list in
-            guard let self else { return }
-            self.list = list
-            self.items.accept(list)
-        }
-        navigationController?.pushViewController(vc, animated: true)
     }
 }
---
+++ PracticeRxSwift/0801/Shopping/ViewModel/ShoppingViewModel.swift
@@ -0,0 +1,88 @@
+//
+//  ShoppingViewModel.swift
+//  PracticeRxSwift
+//
+//  Created by 김정윤 on 8/5/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class ShoppingViewModel {
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let addItem: Observable<ControlProperty<String>.Element>
+        let removeItem: ControlEvent<IndexPath>
+        let renameItem: PublishRelay<(Int, String)>
+        let likeItem: PublishRelay<Int>
+        let bookmarkItem: PublishRelay<Int>
+        let selectedItem: Observable<(ControlEvent<IndexPath>.Element, ControlEvent<Item>.Element)>
+        let searchBtnTapped: ControlEvent<()>?
+        let getNewItemList: PublishRelay<[Item]>
+    }
+    
+    struct Output {
+        var list: [Item]
+        let items: BehaviorRelay<[Item]>
+        let addedItemTrigger: BehaviorRelay<Void>
+        let selectedItem: Observable<(ControlEvent<IndexPath>.Element, ControlEvent<Item>.Element)>
+        let searchBtnTapped: ControlEvent<()>
+    }
+    
+    func transform(_ input: Input) -> Output? {
+        print(#function)
+        var list = ItemList().list
+        let items = BehaviorRelay(value: list)
+        let addedItemTrigger = BehaviorRelay(value: ())
+        guard let tap = input.searchBtnTapped else { return nil }
+        
+        input.addItem
+            .bind(onNext: { itemName in
+                let item = Item(name: itemName)
+                list.insert(item, at: 0)
+                print(list)
+                items.accept(list)
+                addedItemTrigger.accept(())
+            }).disposed(by: disposeBag)
+        
+        input.removeItem
+            .bind { indexPath in
+                print(list)
+                list.remove(at: indexPath.row)
+                print(list)
+                items.accept(list)
+            }.disposed(by: disposeBag)
+        
+        input.renameItem
+            .bind { (idx, newItemName) in
+                print(list)
+                list[idx].name = newItemName
+                items.accept(list)
+                print(list)
+            }.disposed(by: disposeBag)
+        
+        input.likeItem
+            .bind { idx in
+                list[idx].isComplete.toggle()
+                items.accept(list)
+            }.disposed(by: disposeBag)
+        
+        input.bookmarkItem
+            .bind { idx in
+                list[idx].bookmark.toggle()
+                items.accept(list)
+            }.disposed(by: disposeBag)
+        
+        input.getNewItemList
+            .bind { itemList in
+                list = itemList
+                items.accept(list)
+            }.disposed(by: disposeBag)
+        
+        let output = Output(list: list, items: items, addedItemTrigger: addedItemTrigger,
+                            selectedItem: input.selectedItem, searchBtnTapped: tap)
+        return output
+    }
+}
